08dc974302c1965c2a3f9176ce042b076565a73c
Yelyzaveta <yelyzavetamatskiv09@gmail.com>
2024-11-06 00:14:07 +0200

    Add files via upload

---
+++ lab_07/script.js
@@ -0,0 +1,23 @@
+let firstNumber = Number(prompt("Enter the first number"));
+let mathSign = prompt("Enter the math sign");
+let secondNumber = Number(prompt("Enter the first number"));
+
+if (mathSign== '+') {
+    alert(firstNumber + secondNumber);
+
+}
+
+else if (mathSign== '-') {
+    alert(firstNumber - secondNumber);
+}
+
+else if (mathSign== '*') {
+    alert(firstNumber * secondNumber);
+}
+
+else if (mathSign== '/') {
+    alert(firstNumber / secondNumber);
+}
+
+if (mathSign == any);
+
