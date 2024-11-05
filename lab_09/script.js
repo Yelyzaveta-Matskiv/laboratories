b9915dd521ff30f25ee2bb8c1ab3dbb57f7bb8d8
Yelyzaveta <yelyzavetamatskiv09@gmail.com>
2024-11-06 00:16:01 +0200

    Add files via upload

---
+++ lab_09/script.js
@@ -0,0 +1,9 @@
+function toggleFolder(element) {
+    element.classList.toggle('open');
+    event.stopPropagation(); 
+}
+
+function openFile(event, fileName) {
+    alert(fileName);
+    event.stopPropagation(); 
+}
